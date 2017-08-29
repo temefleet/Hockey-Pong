5950128e88ee20530fa46d0874e919783fd4a85b
jefe <jefe@fleet.local>
2017-08-29 10:44:35 -0700

    Made PaddleCentre and PaddleForward subclasses

--- src/partials/PaddleCentre.js
+++ src/partials/PaddleCentre.js
@@ -1,16 +1,12 @@
-import { SVG_NS } from '../settings';
 
-export default class PaddleCentre {
+import PaddleGoalie from './PaddleGoalie';
+
+export default class PaddleCentre extends PaddleGoalie {
 
   constructor(boardHeight, width, height, x, y, up, down, color) {
 
-    this.boardHeight = boardHeight;
-    this.width = width;
-    this.height = height;
-    this.x = x;
+    super(boardHeight, width, height, x);
     this.y = (y - this.height)/2;
-    this.speed = 10;
-    this.score = 0;
     this.color = color;
 
     document.addEventListener('keydown', event => {
@@ -32,28 +28,4 @@ export default class PaddleCentre {
     let bottomYC = y + height;
     return [leftXC, rightXC, topYC, bottomYC];
   }
-
-  up() {
-    this.y = Math.max(this.boardHeight * (1/5), this.y - this.speed);
-  } // end up
-
-  down() {
-    this.y = Math.min(
-      this.boardHeight * (4/5) - this.height, 
-      this.y + this.speed
-    );
-  } // end down
-
-  render(svg) {
-    // render paddle goalie
-    let paddleCentre = document.createElementNS(SVG_NS, 'rect');
-		paddleCentre.setAttributeNS(null, 'width', this.width);
-    paddleCentre.setAttributeNS(null, 'height', this.height);
-    paddleCentre.setAttributeNS(null, 'x', this.x);
-    paddleCentre.setAttributeNS(null, 'y', this.y);
-    paddleCentre.setAttributeNS(null, 'fill', this.color);
-    svg.appendChild(paddleCentre);
-
-  } // end render
-
 } // end Paddle class
--- src/partials/PaddleForwards.js
+++ src/partials/PaddleForwards.js
@@ -1,17 +1,14 @@
-import { SVG_NS } from '../settings';
+import PaddleDefenders from './PaddleDefenders';
 
-export default class PaddleForwards {
+export default class PaddleForwards extends PaddleDefenders {
 
   constructor(boardHeight, width, height, x, y1, y2, up, down, color) {
 
-    this.boardHeight = boardHeight;
-    this.width = width;
+    super(boardHeight, width);
     this.height = height;
     this.x = x;
     this.y1 = y1 * (3/10) - (this.height/2);
     this.y2 = y2 * (7/10) - (this.height/2);
-    this.speed = 10;
-    this.score = 0;
     this.color = color;
 
     document.addEventListener('keydown', event => {
@@ -35,42 +32,5 @@ export default class PaddleForwards {
     let bottomY2F = y2 + height;
     return [leftXF, rightXF, topY1F, bottomY1F, topY2F, bottomY2F];
   }
-
-  up() {
-    this.y1 = Math.max(0, this.y1 - this.speed);
-    this.y2 = Math.max(this.boardHeight * (2/5), this.y2 - this.speed);
-  } // end up
-
-  down() {
-    this.y1 = Math.min(
-      this.boardHeight*(3/5) - this.height, 
-      this.y1 + this.speed
-    );
-    this.y2 = Math.min(
-      this.boardHeight - this.height, 
-      this.y2 + this.speed
-    );
-  } // end down
-
-  render(svg) {
-    // render paddle forward top
-    let paddleForwardTop = document.createElementNS(SVG_NS, 'rect');
-		paddleForwardTop.setAttributeNS(null, 'width', this.width);
-    paddleForwardTop.setAttributeNS(null, 'height', this.height);
-    paddleForwardTop.setAttributeNS(null, 'x', this.x);
-    paddleForwardTop.setAttributeNS(null, 'y', this.y1);
-    paddleForwardTop.setAttributeNS(null, 'fill', this.color);
-    svg.appendChild(paddleForwardTop);
-
-    // paddle forward bottom
-    let paddleForwardBottom = document.createElementNS(SVG_NS, 'rect');
-		paddleForwardBottom.setAttributeNS(null, 'width', this.width);
-    paddleForwardBottom.setAttributeNS(null, 'height', this.height);
-    paddleForwardBottom.setAttributeNS(null, 'x', this.x);
-    paddleForwardBottom.setAttributeNS(null, 'y', this.y2);
-    paddleForwardBottom.setAttributeNS(null, 'fill', this.color);
-    svg.appendChild(paddleForwardBottom);
-
-  } // end render
 
 } // end Paddle class
